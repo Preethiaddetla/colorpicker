663693a2705742eef1cb82d2448a7633b2856854
Preethiaddetla <addetlapreethi@gmail.com>
2024-09-18 13:25:51 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,262 @@
+function myred(){
+    document.getElementById('container').style.backgroundColor='red'
+}
+function myred1(){
+    document.getElementById('container').style.backgroundColor='#ff0040'
+}
+function mypink(){
+    document.getElementById('container').style.backgroundColor='#ff0080'
+}
+function mypink1(){
+    document.getElementById('container').style.backgroundColor='#ff00bf'
+}
+function mypink2(){
+    document.getElementById('container').style.backgroundColor='#ff00ff'
+}
+function myviolet(){
+    document.getElementById('container').style.backgroundColor='#bf00ff'
+}
+function myviolet1(){
+    document.getElementById('container').style.backgroundColor='#8000ff'
+}
+
+function myblue1(){
+    document.getElementById('container').style.backgroundColor='#0000ff'
+}
+function myblue2(){
+    document.getElementById('container').style.backgroundColor='#0080ff'
+}
+function myskyblue(){
+    document.getElementById('container').style.backgroundColor='	#00bfff'
+}
+function myaqua(){
+    document.getElementById('container').style.backgroundColor='	#00ffff'
+}
+function myaquamarine(){
+    document.getElementById('container').style.backgroundColor='	#00ffbf'
+}
+function myaquamarine1(){
+    document.getElementById('container').style.backgroundColor='	#00ff80'
+}
+function mygreen(){
+    document.getElementById('container').style.backgroundColor='	#00ff00'
+}
+function myhazelgreen(){
+    document.getElementById('container').style.backgroundColor='	#617c58'
+}
+function mylightgreen(){
+    document.getElementById('container').style.backgroundColor='	#80ff00'
+}
+function myyellowgreen(){
+    document.getElementById('container').style.backgroundColor='	#bfff00'
+}
+function myyellow(){
+    document.getElementById('container').style.backgroundColor='#ffff00'
+}
+function mymustard(){
+    document.getElementById('container').style.backgroundColor='#ffbf00'
+}
+function myorange(){
+    document.getElementById('container').style.backgroundColor='#ff8000'
+}
+function myorange1(){
+    document.getElementById('container').style.backgroundColor='#ff4000'
+}
+function mycottoncandy(){
+    document.getElementById('container').style.backgroundColor='#fcdfff'
+}
+function mycarnationpink(){
+    document.getElementById('container').style.backgroundColor='#f778a1'
+}
+function myhotpink(){
+    document.getElementById('container').style.backgroundColor='#f660ab'
+}
+function mythistle(){
+    document.getElementById('container').style.backgroundColor='#d2b9d3 '
+}
+function myseashell(){
+    document.getElementById('container').style.backgroundColor='#fff5ee'
+}
+function myvanilla(){
+    document.getElementById('container').style.backgroundColor='#f3e5ab'
+}
+function myclovergreen(){
+    document.getElementById('container').style.backgroundColor='#3ea055'
+}
+function myteal(){
+    document.getElementById('container').style.backgroundColor='#008080 '
+}
+function myblack(){
+    document.getElementById('container').style.backgroundColor='#000000 '
+}
+function mywhite(){
+    document.getElementById('container').style.backgroundColor='#ffffff '
+}
+function mybrown(){
+    document.getElementById('container').style.backgroundColor='#560319 '
+}
+
+
+
+
+function mytextred(){
+    document.getElementById('text').style.color='red'
+}
+function mytextred1(){
+    document.getElementById('text').style.color='#ff0040'
+}
+function mytextpink(){
+    document.getElementById('text').style.color='#ff0080'
+}
+function mytextpink1(){
+    document.getElementById('text').style.color='#ff00bf'
+}
+function mytextpink2(){
+    document.getElementById('text').style.color='#ff00ff'
+}
+function mytextviolet(){
+    document.getElementById('text').style.color='#bf00ff'
+}
+function mytextviolet1(){
+    document.getElementById('text').style.color='#8000ff'
+}
+
+function mytextblue1(){
+    document.getElementById('text').style.color='#0000ff'
+}
+function mytextblue2(){
+    document.getElementById('text').style.color='#0080ff'
+}
+function mytextskyblue(){
+    document.getElementById('text').style.color='	#00bfff'
+}
+function mytextaqua(){
+    document.getElementById('text').style.color='	#00ffff'
+}
+function mytextaquamarine(){
+    document.getElementById('text').style.color='	#00ffbf'
+}
+function mytextaquamarine1(){
+    document.getElementById('text').style.color='	#00ff80'
+}
+function mytextgreen(){
+    document.getElementById('text').style.color='	#00ff00'
+}
+function mytexthazelgreen(){
+    document.getElementById('text').style.color='	#617c58'
+}
+function mytextlightgreen(){
+    document.getElementById('text').style.color='	#80ff00'
+}
+function mytextyellowgreen(){
+    document.getElementById('text').style.color='	#bfff00'
+}
+function mytextyellow(){
+    document.getElementById('text').style.color='#ffff00'
+}
+function mytextmustard(){
+    document.getElementById('text').style.color='#ffbf00'
+}
+function mytextorange(){
+    document.getElementById('text').style.color='#ff8000'
+}
+function mytextorange1(){
+    document.getElementById('text').style.color='#ff4000'
+}
+function mytextcottoncandy(){
+    document.getElementById('text').style.color='#fcdfff'
+}
+function mytextcarnationpink(){
+    document.getElementById('text').style.color='#f778a1'
+}
+function mytexthotpink(){
+    document.getElementById('text').style.color='#f660ab'
+}
+function mytextthistle(){
+    document.getElementById('text').style.color='#d2b9d3 '
+}
+function mytextseashell(){
+    document.getElementById('text').style.color='#fff5ee'
+}
+function mytextvanilla(){
+    document.getElementById('text').style.color='#f3e5ab'
+}
+function mytextclovergreen(){
+    document.getElementById('text').style.color='#3ea055'
+}
+function mytextteal(){
+    document.getElementById('text').style.color='#008080 '
+}
+function mytextblack(){
+    document.getElementById('text').style.color='#000000 '
+}
+function mytextwhite(){
+    document.getElementById('text').style.color='#ffffff '
+}
+function mytextbrown(){
+    document.getElementById('text').style.color='#560319 '
+}
+
+
+
+function myfont1() {
+    document.getElementById('text').style.fontFamily = 'Times New Roman, serif';
+}
+
+function myfont2() {
+    document.getElementById('text').style.fontFamily = '"Lucida Console", "Courier New", monospace';
+}
+
+function myfont3() {
+    document.getElementById('text').style.fontFamily = 'cursive';
+}
+
+function myfont4() {
+    document.getElementById('text').style.fontFamily = 'Cambria, Cochin, Georgia, Times, "Times New Roman", serif';
+}
+
+function myfont5() {
+    document.getElementById('text').style.fontFamily = "'Gill Sans', 'Gill Sans MT', Calibri, 'Trebuchet MS', sans-serif";
+}
+function myfont6() {
+    document.getElementById('text').style.fontFamily = "Fantasy";
+}
+function myfont7() {
+    document.getElementById('text').style.fontFamily = "'Brush Script MT', cursive";
+}
+function myfont8() {
+    document.getElementById('text').style.fontFamily = " Garamond, serif";
+}
+function myfont9() {
+    document.getElementById('text').style.fontFamily = " Copperplate, Papyrus, fantasy";
+}
+
+function myfont10() {
+    document.getElementById('text').style.fontFamily = " Perpetua, serif";
+}
+function myfont11() {
+    document.getElementById('text').style.fontFamily = " Didot, serif";
+}
+function myfont12() {
+    document.getElementById('text').style.fontFamily = " Impact, Haettenschweiler, 'Arial Narrow Bold', sans-serif";
+}
+function myfont13() {
+    document.getElementById('text').style.fontFamily = " 'Trebuchet MS', 'Lucida Sans Unicode', 'Lucida Grande', 'Lucida Sans', Arial, sans-serif";
+}
+function myfont14() {
+    document.getElementById('text').style.fontFamily = " 'Segoe UI', Tahoma, Geneva, Verdana, sans-serif";
+}
+
+let btn=document.getElementById('btn')
+
+btn.addEventListener('click',myfun)
+
+
+function myfun(){
+    html2canvas(document.getElementById('container')).then(canvas => {
+        const link=document.createElement('a')
+        link.href=canvas.toDataURL('image/png')
+        link.download='image.png'
+        link.click();  
+    });
+}
